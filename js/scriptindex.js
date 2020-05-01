8bab1fbe72d9b0c5ccbcbb07169edd8ea2d8e23c
oleeggg <64087472+oleeggg@users.noreply.github.com>
2020-05-01 14:12:01 +0300

    Add files via upload

---
+++ js/scriptindex.js
@@ -0,0 +1,8 @@
+const parllax = document.querySelector(".parallax");
+
+window.addEventListener("scroll", function() {
+  let offset = window.pageYOffset;
+  parllax.style.backgroundPositionY = offset * 0.5 + 'px';
+});
+
+
